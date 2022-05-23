8b03fe8f1c79095d790a4770396a5313d7a1273b
vitormiura <vitormiura3@gmail.com>
2022-05-23 16:56:46 -0300

    pyqt games att

--- fund-programacao/Python/pyqt/main.py
+++ fund-programacao/Python/pyqt/main.py
@@ -1,19 +1,22 @@
 from PyQt5 import QtWidgets, uic 
 import sys
+from telas import velha
 
 class Ui(QtWidgets.QMainWindow):
     def __init__(self):
         super(Ui, self).__init__()
         uic.loadUi('telas/main.ui', self)
         
-        self.button = self.findChild(QtWidgets.QPushButton, 'pushButton_2')
+        self.button = self.findChild(QtWidgets.QPushButton, 'pushButton_3')
         self.button.clicked.connect(self.printButtonPressed)
         
 
         self.show()
         
     def printButtonPressed(self):
-        print('macoknha')
+        print('butao')
+        velha = velha.velha_class()
+        
 
 app = QtWidgets.QApplication(sys.argv)
 
