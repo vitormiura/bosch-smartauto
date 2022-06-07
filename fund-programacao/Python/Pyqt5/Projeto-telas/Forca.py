e1314bb76652d5b88d66d198ecd7f6fc436435bd
vitormiura <vitormiura3@gmail.com>
2022-06-07 16:53:17 -0300

    add hangman game to pyqt minigames exercise

--- fund-programacao/Python/Pyqt5/Projeto-telas/Forca.py
+++ fund-programacao/Python/Pyqt5/Projeto-telas/Forca.py
@@ -1,238 +1,258 @@
 import random
+
 from PyQt5 import QtCore, QtGui, QtWidgets
-import sys
-  
-jogando = True
-vida = 7
-dig = []
+
+posicao = 70
+arquivo = ""
+selecionada = ""
+linha = []
+palavra = ""
+palavraOut = ""
+vidas = 6
+dica = ""
+dict = ""
 
 class Ui_MainWindow(object):
-    
-    def monke(self):
-        self.vida1.setVisible(False)
-        self.vida2.setVisible(False)
-        self.vida3.setVisible(False)
-        self.vida4.setVisible(False)
-        self.vida5.setVisible(False)
-        self.vida6.setVisible(False)
-    
-    def lifes(self, vida):
-        if vida == 6:
-            self.vida1.setVisible(True)
-        elif vida == 5:
-            self.vida1.setVisible(False)
-            self.vida2.setVisible(True)
-        elif vida == 4:
-            self.vida2.setVisible(False)
-            self.vida3.setVisible(True)
-        elif vida == 3:
-            self.vida3.setVisible(False)
-            self.vida4.setVisible(True)
-        elif vida == 2:
-            self.vida4.setVisible(False)
-            self.vida5.setVisible(True)
-        elif vida == 1:
-            self.vida5.setVisible(False)
-            self.vida6.setVisible(True)
-    
-        
-    def init(self):
-        global dig, vida, word
-        print(word)
-        self.dica.setText(word[1])
-        
-        while True:
-            temp = ''
-            letra = self.input.text()
-            self.send.setText(input)
-            dig.append(letra)
 
-            for i in word[0]:
-                if i in dig:
-                    temp += i
-                else:
-                    temp += "-"
-                    
-            self.mask.setText(temp)
-            
-            if letra not in word[0]:
-                vida -= 1     
-                self.monke(vida)      
-    
+    global palavra
+    global palavraOut
+
+
     def setupUi(self, MainWindow):
+        global posicao
+        global vidas
+        global dica
+        global dict
+        dict = selecionarPalavra(abrirArquivo())
+
+
+        for i in dict.keys():
+            palavra = i
+        palavraOut = preencherVetor(palavra)
+
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(341, 556)
+        MainWindow.resize(541, 505)
         MainWindow.setStyleSheet("background-color: rgb(250, 250, 250);\n"
-        "background-color: rgb(85, 0, 127);")
+                                 "background-color: rgb(85, 0, 127);")
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
+
         self.vida1 = QtWidgets.QLabel(self.centralwidget)
         self.vida1.setGeometry(QtCore.QRect(50, 70, 231, 301))
         self.vida1.setText("")
-        self.vida1.setPixmap(QtGui.QPixmap("fund-programacao\Python\pyqt\telas\hangmonkey\macacobraco1.png"))
+        self.vida1.setPixmap(QtGui.QPixmap("hangmonkey/macacobraco1.png"))
         self.vida1.setScaledContents(True)
         self.vida1.setObjectName("vida1")
+
         self.vida2 = QtWidgets.QLabel(self.centralwidget)
         self.vida2.setGeometry(QtCore.QRect(50, 70, 231, 301))
         self.vida2.setText("")
         self.vida2.setPixmap(QtGui.QPixmap("hangmonkey/macacobracos.png"))
         self.vida2.setScaledContents(True)
         self.vida2.setObjectName("vida2")
+
         self.vida3 = QtWidgets.QLabel(self.centralwidget)
         self.vida3.setGeometry(QtCore.QRect(50, 70, 231, 301))
         self.vida3.setText("")
         self.vida3.setPixmap(QtGui.QPixmap("hangmonkey/macacocabeca.png"))
         self.vida3.setScaledContents(True)
         self.vida3.setObjectName("vida3")
+
         self.vida4 = QtWidgets.QLabel(self.centralwidget)
         self.vida4.setGeometry(QtCore.QRect(50, 70, 231, 301))
         self.vida4.setText("")
         self.vida4.setPixmap(QtGui.QPixmap("hangmonkey/macacocorpo.png"))
         self.vida4.setScaledContents(True)
         self.vida4.setObjectName("vida4")
+
         self.vida5 = QtWidgets.QLabel(self.centralwidget)
         self.vida5.setGeometry(QtCore.QRect(50, 70, 231, 301))
         self.vida5.setText("")
         self.vida5.setPixmap(QtGui.QPixmap("hangmonkey/macacoperna1.png"))
         self.vida5.setScaledContents(True)
         self.vida5.setObjectName("vida5")
+
         self.vida6 = QtWidgets.QLabel(self.centralwidget)
         self.vida6.setGeometry(QtCore.QRect(50, 70, 231, 301))
         self.vida6.setText("")
         self.vida6.setPixmap(QtGui.QPixmap("hangmonkey/macacomorto.png"))
         self.vida6.setScaledContents(True)
         self.vida6.setObjectName("vida6")
+
         self.element = QtWidgets.QLabel(self.centralwidget)
         self.element.setGeometry(QtCore.QRect(100, 10, 131, 41))
         self.element.setStyleSheet("border-radius: 15px;\n"
-        "background-color: rgb(0, 0, 0);")
+                                   "background-color: rgb(0, 0, 0);")
         self.element.setText("")
         self.element.setObjectName("element")
+
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(110, 20, 81, 21))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.label_2.setFont(font)
         self.label_2.setStyleSheet("background-color: none;\n"
-        "color: white;")
+                                   "color: white;")
+
         self.label_2.setTextFormat(QtCore.Qt.MarkdownText)
         self.label_2.setObjectName("label_2")
+        # VIDAS -------------------------------------
         self.vidasTotal = QtWidgets.QLabel(self.centralwidget)
         self.vidasTotal.setGeometry(QtCore.QRect(190, 20, 16, 21))
         font = QtGui.QFont()
         font.setPointSize(16)
         self.vidasTotal.setFont(font)
         self.vidasTotal.setStyleSheet("background-color: none;\n"
-        "color: white;")
+                                      "color: white;")
         self.vidasTotal.setText("")
         self.vidasTotal.setTextFormat(QtCore.Qt.MarkdownText)
         self.vidasTotal.setObjectName("vidasTotal")
+
         self.element_2 = QtWidgets.QLabel(self.centralwidget)
-        self.element_2.setGeometry(QtCore.QRect(20, 430, 301, 41))
+        self.element_2.setGeometry(QtCore.QRect(20, 380, 301, 41))
         self.element_2.setStyleSheet("border-radius: 15px;\n"
-        "background-color: rgb(0, 0, 0);")
+                                    )
         self.element_2.setText("")
         self.element_2.setObjectName("element_2")
+
         self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(30, 440, 41, 20))
+        self.label_4.setGeometry(QtCore.QRect(30, 390, 91, 30))
+
         font = QtGui.QFont()
         font.setPointSize(10)
         font.setBold(True)
         font.setUnderline(True)
         font.setWeight(75)
+
         self.label_4.setFont(font)
         self.label_4.setStyleSheet("background-color: none;\n"
-        "color: white;")
+                                   "color: white;")
         self.label_4.setTextFormat(QtCore.Qt.MarkdownText)
         self.label_4.setObjectName("label_4")
+        pegarDica()
+        # LABEL PALAVRA--------------------------------------------
+        self.labelPa = QtWidgets.QLabel(self.centralwidget)
+        self.labelPa.setGeometry(QtCore.QRect(40, 90, 231, 40))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.labelPa.setFont(font)
+        self.labelPa.setStyleSheet("background-color: green;\n"
+                                "color: white;")
+        self.labelPa.setText("")
+        self.labelPa.setTextFormat(QtCore.Qt.MarkdownText)
+        self.labelPa.setObjectName("palavraLabel")
+        self.labelPa.setText("DICA: "+dica)
+
+        # DICA -----------------------------------------------
         self.dica = QtWidgets.QLabel(self.centralwidget)
-        self.dica.setGeometry(QtCore.QRect(70, 440, 231, 20))
+        self.dica.setGeometry(QtCore.QRect(40, 90, 231, 10))
         font = QtGui.QFont()
         font.setPointSize(10)
         self.dica.setFont(font)
         self.dica.setStyleSheet("background-color: none;\n"
-        "color: white;")
+                                "color: white;")
         self.dica.setText("")
         self.dica.setTextFormat(QtCore.Qt.MarkdownText)
-        self.dica.setObjectName("dica")
-        self.input = QtWidgets.QLineEdit(self.centralwidget)
-        self.input.setGeometry(QtCore.QRect(60, 480, 181, 41))
-        self.input.setStyleSheet("background-color: rgb(255, 255, 255);\n"
-        "border-radius: 15px;")
-        self.input.setObjectName("input")
+
+        # INPUT BOX ------------------------------------------
+
+        for i in range(0, len(palavraOut)):
+
+            nome = "label",i
+            posicao = posicao + 30
+            self.nome = QtWidgets.QLabel(self.centralwidget)
+            self.nome.setGeometry(QtCore.QRect(posicao, 390, 20, 20))
+            self.nome.setFont(font)
+            self.nome.setStyleSheet("background-color: green;\n"
+                                       "color: white;")
+            self.nome.setTextFormat(QtCore.Qt.MarkdownText)
+            self.nome.setObjectName("label"+str(i))
+
+        # BOTAO ENVIAR---------------------------------------
         self.send = QtWidgets.QPushButton(self.centralwidget)
-        self.send.setGeometry(QtCore.QRect(250, 480, 61, 41))
+        self.send.setGeometry(QtCore.QRect(250, 430, 61, 41))
         self.send.setStyleSheet("border-radius: 15px;\n"
-        "border-color:1px rgb(0, 0, 0);\n"
-        "background-color: rgb(213, 213, 213);\n"
-        "")
+                                "border-color:1px rgb(0, 0, 0);\n"
+                                "background-color: rgb(213, 213, 213);\n"
+                                "")
         self.send.setObjectName("send")
-        self.mask = QtWidgets.QLabel(self.centralwidget)
-        self.mask.setGeometry(QtCore.QRect(40, 390, 231, 20))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        self.mask.setFont(font)
-        self.mask.setStyleSheet("background-color: none;\n"
-        "color: white;")
-        self.mask.setText("")
-        self.mask.setTextFormat(QtCore.Qt.MarkdownText)
-        self.mask.setObjectName("mask")
-        self.element_3 = QtWidgets.QLabel(self.centralwidget)
-        self.element_3.setGeometry(QtCore.QRect(20, 380, 301, 41))
-        self.element_3.setStyleSheet("border-radius: 15px;\n"
-        "background-color: rgb(0, 0, 0);")
-        self.element_3.setText("")
-        self.element_3.setObjectName("element_3")
-        self.element_3.raise_()
-        self.vida1.raise_()
-        self.vida2.raise_()
-        self.vida3.raise_()
-        self.vida4.raise_()
-        self.vida5.raise_()
-        self.vida6.raise_()
-        self.element.raise_()
-        self.label_2.raise_()
-        self.vidasTotal.raise_()
-        self.element_2.raise_()
-        self.label_4.raise_()
-        self.dica.raise_()
-        self.input.raise_()
-        self.send.raise_()
-        self.mask.raise_()
+        self.send.clicked.connect(chutarLetra(self.label_2, "A"))
+        # ---------------------------------------------------
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+        #LABEL LETRA
+        #------------------------------------------------------
+        self.caixaLetra = QtWidgets.QLineEdit(self.centralwidget)
+        self.caixaLetra.setGeometry(265, 350, 30, 30)
+        self.send.setObjectName("caixaLetra")
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_2.setText(_translate("MainWindow", "VIDAS:"))
-        self.label_4.setText(_translate("MainWindow", "DICA:"))
+        self.label_2.setText(_translate("MainWindow", "VIDAS:"+str(vidas)))
+        self.label_4.setText(_translate("MainWindow", "Palavra:"))
         self.send.setText(_translate("MainWindow", "SEND"))
 
-class Forca(Ui_MainWindow):
-    def __init__(self, forca):
-        self.setupUi(forca)
-        self.mask.setText(self.__sortear)
-        
-    @staticmethod
-    def __sortear():
-        f = open("fund-programacao\Python\pyqt\palavras.txt", "r").read() 
-        separe = f.replace('\n', '').split(";")   
-        sort = separe[random.randint(0, len(separe)-1)] #SORTEIO DA PALAVRA! 
-        w = sort.split(":")
-        tam = len(word[0])
-        
-        return word
-    
-    
-    
-    
+def preencherVetor(palavra):
+    palavraOut = []
+    for i in range(len(palavra)):
+        palavraOut.append(" _ ")
+    return palavraOut
+
+def abrirArquivo():
+    file = open("palavras.txt")
+    return file
+
+def selecionarPalavra(file):
+    linha = file.readlines()
+    palavraAux = linha[random.randint(0, len(linha) - 1)]
+    dicionario = {}
+    chave = palavraAux.split(":")
+    dicionario[chave[0]] = chave[1]
+    return dicionario
+def pegarDica():
+    global dica
+    for i in dict.values():
+        dica = i
+
+def chutarLetra(label, letra):
+    x = 0
+    f = 7
+    global vidas
+    print(letra)
+    print(label)
+
+    for i in dict.values():
+        dica = i
+    while (vidas > 0):
+
+        for i in range(0, len(palavra)):
+            if palavra[i] == letra:
+                palavraOut[i] = letra
+
+        if letra not in palavra:
+            f = f - 1
+            if f == 2:
+                f = f - 1
+
+
+
+            if f == 4:
+                vidas = vidas - 1
+
+        if " _ " not in palavraOut:
+            print("Parabéns você Ganhou!!!")
+            break
+
+
+
 if __name__ == "__main__":
+    import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = Ui_MainWindow()
