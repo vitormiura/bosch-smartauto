226d74be6ef3a73f371f0fd92daddfb3f9b4b084
vitormiura <vitormiura3@gmail.com>
2022-08-31 16:56:00 -0300

    Update java proj

---
+++ fund-programacao/Java/University/App/src/Pessoas/Membros.java
@@ -0,0 +1,21 @@
+package Pessoas;
+
+public class Membros {
+    private String name;
+    private int age;
+    private int register;
+    private String email;
+
+
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+    
+    public int getRegister(){
+        return register;
+    }
+}
