80df954de981c941ebf43c46adb8d30c430fcdb1
vitormiura <vitormiura3@gmail.com>
2022-09-12 16:17:31 -0300

    Update java DAO

--- fund-programacao/Java/University/App/src/DAO.java
+++ fund-programacao/Java/University/App/src/DAO.java
@@ -3,10 +3,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DAO {
-    public Connection DBconnect(){
+    public Connection DBconnect() throws SQLException{
         Connection con = null;
         try{
-            String url = "jdbc:postgresql://clpuectczmwtno:57620ac3a3eeb990ce187fd1f1751c21db4d57f7d8758f3f38a19a474a9251d4@ec2-34-234-240-121.compute-1.amazonaws.com:5432/deu7o7mk1t1if";
+            String url = "jdbc:mysql://127.0.0.1:3306/universidade?user=root&password=";
             con = DriverManager.getConnection(url);
         } catch (SQLException error){
             System.err.println(error.getMessage());
